7195f13d73be25d42dc60cf79222971b21b35e43
tim <romonath@students.uni-marburg.de>
2023-07-12 13:56:44 +0200

    implemented first guess of pcm plotter

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import data.WaveObjectV1;
+import graphics.PlotterV1;
+import graphics.PlotterV1Impl;
 import waveIO.FileReaderV1;
 import waveIO.FileReaderV1Impl;
 import waveIO.FileWriterV1;
@@ -6,5 +8,27 @@ import waveIO.FileWriterV1Impl;
 
 public class Main {
     public static void main(String[] args){
+        FileReaderV1 fr = new FileReaderV1Impl();
+        FileWriterV1 fw = new FileWriterV1Impl();
+        WaveObjectV1 wo;
+
+        try {
+            wo  = fr.read("C:\\Users\\timro\\IdeaProjects\\Bachelor\\rec\\C418-Minecraft.wav");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        wo.setName("soundoutput_test.wav");
+
+        PlotterV1 plotter = new PlotterV1Impl();
+
+        plotter.plotPCM(wo);
+
+  //      try {
+  //          fw.write("C:\\Users\\timro\\IdeaProjects\\Bachelor\\rec\\", wo);
+  //      } catch (Exception e) {
+  //          throw new RuntimeException(e);
+  //      }
+  //      System.out.println("");
     }
 }
--- src/graphics/PlotterPCM.java
+++ src/graphics/PlotterPCM.java
@@ -2,18 +2,56 @@ package graphics;
 
 import data.WaveObjectV1;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 public class PlotterPCM {
     public static BufferedImage plott(WaveObjectV1 wo, int sizeX, int sizeY) {
+        double[] javaPCM = wo.getJavaPCM();
 
-        return null;
+        //calculate the stepsize for x
+        int stepsize = javaPCM.length / sizeX;
+        //get the scale for y
+        int scale = (int) (getHighestValue(javaPCM) / sizeY) / 2;
+
+        BufferedImage output = new BufferedImage(sizeX, sizeY,BufferedImage.TYPE_4BYTE_ABGR);
+
+        double currentValue;
+
+        for(int x = 0; x < sizeX; x++){
+            currentValue = getAverageOf(Arrays.copyOfRange(javaPCM, x * stepsize, (x + 1) * stepsize - 1));
+
+            for(int y = 0; y < sizeY; y++){
+                if (currentValue > 0 && y >= sizeY/2) {
+                    if (sizeY / 2 - y < currentValue * scale) output.setRGB(x,y, Color.BLACK.getRGB());
+                }
+                if (currentValue <= 0) {
+                    if (y > currentValue * scale) output.setRGB(x,y, Color.BLACK.getRGB());
+                }
+            }
+        }
+    //todo image is black
+        return output;
     }
 
-    private double getHighestValue(double[] arr){
+    private static double getHighestValue(double[] arr){
         double output = 0;
-        for(int n = 0; n < arr.length; n++){
+        double current;
 
+        for(int n = 0; n < arr.length; n++){
+            current = Math.abs(arr[n]);
+            if(current > output) output = current;
         }
+
+        return output;
+    }
+
+    private static double getAverageOf(double[] arr){
+        double output = 0;
+
+        for (double d:arr) output += d;
+
+        return output / arr.length;
     }
 }
