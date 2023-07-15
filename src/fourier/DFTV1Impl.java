d366189a678744aca1dea58bee191cf59a4bc7bc
tim <romonath@students.uni-marburg.de>
2023-07-15 12:30:56 +0200

    tested dft

--- src/fourier/DFTV1Impl.java
+++ src/fourier/DFTV1Impl.java
@@ -7,15 +7,24 @@ import data.WaveObjectV1;
 public class DFTV1Impl implements DFTV1{
     @Override
     public FourierObjectV1 dft(WaveObjectV1 wo) {
-        int dataLength = wo.getJavaPCM().length;
+        //int dataLength = wo.getJavaPCM().length;
+        int dataLength = 10000;
         double[] real = new double[dataLength], img = new double[dataLength], amplitude = new double[dataLength];
 
+        Integer temp = 0;
+        Integer newTemp = 0;
+        System.out.println("Transform...");
         for(int output = 0; output < dataLength; output++) {//for each output
+            newTemp = (100 * output) / dataLength;
+            if(!temp.equals(newTemp)){
+                System.out.println(temp+" %");
+                temp = newTemp;
+            }
             real[output] = 0;
             img[output] = 0;
-            for(int input = 0; input < 20000; input++){//do for every input
+            for(int input = 0; input < wo.getJavaPCM().length; input++){//do for every input
                 real[output] += wo.getJavaPCM()[input] * Math.cos(2 * Math.PI * input * output / dataLength);
-                real[output] -= wo.getJavaPCM()[input] * Math.sin(2 * Math.PI * input * output / dataLength);
+                img[output] -= wo.getJavaPCM()[input] * Math.sin(2 * Math.PI * input * output / dataLength);
             }
         }
 
--- src/graphics/PlotterFourier.java
+++ src/graphics/PlotterFourier.java
@@ -19,37 +19,34 @@ public class PlotterFourier {
         //calculate the stepsize for x
         int stepsize = fo.getAmplitude().length / sizeX;
 
-        //calculate Plott-Array (datapoints to plot)
-        double[] plot = new double[sizeX];
+        double[] plottData = new double[sizeX];
 
-        //fill Plott-Array
-        for(int i = 0; i < plot.length; i++){
-            plot[i] = getWeightedAverageOf(Arrays.copyOfRange(fo.getAmplitude(), i * stepsize, (i + 1) * stepsize - 1));
+        for(int i = 0; i < plottData.length; i++){
+            plottData[i] = getHighestValue(Arrays.copyOfRange(fo.getAmplitude(),i,i+stepsize));
         }
-
         //get the scale for y
-        double scale = sizeY / getHighestValue(plot) / 2;
+        double scale = sizeY / getHighestValue(plottData);
 
         BufferedImage output = new BufferedImage(sizeX, sizeY,BufferedImage.TYPE_4BYTE_ABGR);
 
         //for each x and y plot if data is in range
         for(int x = 0; x < sizeX; x++){
-            for(int y = 0; y < sizeY / 2; y++){
-                if(sizeY / 2 - y <= plot[x] * scale){
+            for(int y = 0; y < sizeY; y++){
+                if(sizeY - y <= plottData[x] * scale){
                     output.setRGB(x,y, Color.BLACK.getRGB());
-                    output.setRGB(x,sizeY - (y + 1), Color.BLACK.getRGB());
                 }
                 else {
                     output.setRGB(x, y, Color.white.getRGB());
-                    output.setRGB(x, sizeY - (y + 1), Color.white.getRGB());
                 }
             }
         }
+
         return output;
     }
 
     //
     private static double getHighestValue(double[] arr){
+
         double output = 0;
         double current;
 
